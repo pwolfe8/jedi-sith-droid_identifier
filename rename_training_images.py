cda380246f2bc4ee5cbb9f50be8a7153e300c633
Philip Wolfe <pwolfe854@gmail.com>
2017-01-09 03:41:16 -0500

    now classifies Jedi, Sith, or Droid

--- rename_training_images.py
+++ rename_training_images.py
@@ -1,20 +1,16 @@
-#for renaming the bulk image download
+# quick script i made for renaming the bulk image download
+# training data to something reasonable and consistent
 import shutil, os, sys
-image_folder = sys.argv[1]
+image_dir = sys.argv[1]
 
-print('rename images in folder: ' + image_folder + 'this includes: \n')
-print(os.listdir(image_folder))
+print('rename images in folder: ' + image_dir + '  This includes: \n')
+print(os.listdir(image_dir))
 correct = raw_input("Would you like to continue? [y/n] ")
 if(correct == 'y'):
-	counter = 0
-	for name in os.listdir(image_folder):
-		shutil.move(name, 'img' + str(counter) + '.jpg')
-		counter += 1	
+	counter = 1
+	for name in os.listdir(image_dir):
+		filename = '\'' + str(name) + '\'' #put quotes around in case there are spaces in file name
+		os.system('mv ' + image_dir+'/'+filename + ' ' + image_dir+'/'+'img'+str(counter)+'.jpg') #rename 
+		counter += 1
 	print('completed')
-
-
 print('exiting')
-
-
-
-
